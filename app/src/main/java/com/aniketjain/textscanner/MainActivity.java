16c80a469d44051b00bbc8a9a4f21bdfb2f702f2
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-10 22:35:19 +0530

    master

--- app/src/main/java/com/aniketjain/textscanner/MainActivity.java
+++ app/src/main/java/com/aniketjain/textscanner/MainActivity.java
@@ -1,14 +1,47 @@
 package com.aniketjain.textscanner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.aniketjain.textscanner.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static final int REQUEST_CAMERA_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        Binding SetUp
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // we are checking permission for CAMERA Access.
+        checkPermissions();
+
+        onClickListeners();
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, REQUEST_CAMERA_CODE);
+        }
+    }
+
+
+    private void onClickListeners() {
+        binding.captureBtn.setOnClickListener(view -> {
+
+        });
     }
 }
