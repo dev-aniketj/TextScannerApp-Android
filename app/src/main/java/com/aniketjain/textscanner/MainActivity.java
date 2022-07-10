844237f30ce21426d1cbd9e4ecdb1f82182c924c
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-10 23:17:05 +0530

    master

--- app/src/main/java/com/aniketjain/textscanner/MainActivity.java
+++ app/src/main/java/com/aniketjain/textscanner/MainActivity.java
@@ -1,20 +1,30 @@
 package com.aniketjain.textscanner;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.aniketjain.textscanner.databinding.ActivityMainBinding;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private static final int REQUEST_CAMERA_CODE = 100;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +42,41 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+            ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.CAMERA
             }, REQUEST_CAMERA_CODE);
         }
     }
 
-
     private void onClickListeners() {
         binding.captureBtn.setOnClickListener(view -> {
+            CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(this);
 
         });
+    }
+
+    /**
+     * for get the capture button Bitmap result.
+     *
+     * @param requestCode -
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CAMERA_CAPTURE_PERMISSIONS_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri uri = (result != null) ? result.getUri() : null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void getTextFromImage(Bitmap bitmap) {
+
     }
 }
